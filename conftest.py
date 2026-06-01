8d0889d5c2c7d5fab263344187577da745b81f76
mismailova <meryem.ismailova@yandex.ru>
2026-06-01 23:46:24 +0500

    Добавлена фикстура на удаление курьера в тест создания курьера

--- conftest.py
+++ conftest.py
@@ -13,3 +13,15 @@ def courier():
     courier_id = login_response.json()['id']
     yield response, courier_data, courier_id
     CourierMethods().delete_courier(courier_id)
+
+@pytest.fixture()
+def delete_courier():
+    created_courier_data = []
+    yield created_courier_data
+    for courier_data in created_courier_data:
+        login_response = CourierMethods().login_courier({
+            "login": courier_data["login"],
+            "password": courier_data["password"]
+        })
+        courier_id = login_response.json()["id"]
+        CourierMethods().delete_courier(courier_id)
--- tests/courier_tests/test_create_courier.py
+++ tests/courier_tests/test_create_courier.py
@@ -9,22 +9,16 @@ class TestCreateCourier:
     @allure.title('Успешное создание курьера')
     @allure.description('Создается новый курьер при отправке валидного тела запроса')
 
-    def test_create_courier_success(self):
+    def test_create_courier_success(self, delete_courier):
         courier = CourierMethods()
         courier_data = data.generate_courier_data()
         response = courier.create_courier(courier_data)
+        delete_courier.append(courier_data)
 
-        try:
-            assert response.status_code == 201 and response.json() == data.CREATE_COURIER_RESPONSE, (
-                f"Ожидался статус 201 и тело {data.CREATE_COURIER_RESPONSE}, "
-                f"получено: статус {response.status_code}, тело {response.json()}"
+        assert response.status_code == 201 and response.json() == data.CREATE_COURIER_RESPONSE, (
+            f"Ожидался статус 201 и тело {data.CREATE_COURIER_RESPONSE}, "
+            f"получено: статус {response.status_code}, тело {response.json()}"
         )
-        finally:
-            login_response = courier.login_courier({
-                "login": courier_data["login"],
-                "password": courier_data["password"]
-            })
-            courier.delete_courier(login_response.json()["id"])
 
     @allure.title('Ошибка при создании курьера с существующими данными')
     @allure.description('При создании курьера с существующими данными код 409 и текст ошибки')
